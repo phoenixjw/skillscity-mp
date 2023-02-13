d9f2352aa9e4ca9e4f0b1ad2aeb5210319912ae8
phoenixjw <phoenixjw9320@gmail.com>
2023-02-13 18:48:26 +0000

    Changes to re set up data base, still not working

--- pages/api/books.js
+++ pages/api/books.js
@@ -1,25 +1,33 @@
-export default function handler(req, res) {
-    const books = [
-        {   
-            id:"1",
-            artist: "Josh Baker",
-            title: "Information overload",
-            imgUrl: "/assets/informationOve.jpg"
-        },
-        {   
-            id:"2",
-            artist: "Ingi Visons",
-            title: "RJG",
-            imgUrl: "/assets/ingi.jpg"
-        },
-        {   
-            id:"3",
-            artist: "Sebo K",
-            title: "Brock wild",
-            imgUrl: "/assets/seboK.jpg"
-        }
+import sql from "@/utils/postgres";
 
-    ];
+export default async function handler(req, res) {
+    const books = await sql `
+    select * from tunes
+    
+    `;
+
+
+    // const books = [
+    //     {   
+    //         id:"1",
+    //         artist: "Josh Baker",
+    //         title: "Information overload",
+    //         imgUrl: "/assets/informationOve.jpg"
+    //     },
+    //     {   
+    //         id:"2",
+    //         artist: "Ingi Visons",
+    //         title: "RJG",
+    //         imgUrl: "/assets/ingi.jpg"
+    //     },
+    //     {   
+    //         id:"3",
+    //         artist: "Sebo K",
+    //         title: "Brock wild",
+    //         imgUrl: "/assets/seboK.jpg"
+    //     }
+
+    // ];
 
     res.json(books);
 }
--- utils/postgres.js
+++ utils/postgres.js
@@ -1,5 +1,4 @@
 import postgres from "postgres";
-
 const sql = postgres(process.env.POSTGRES_CONNECTION_STRING)
 
-export default sql;
+export default sql
