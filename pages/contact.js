ccac4698f924afec6a6598726e4d1944d231abc1
phoenixjw <phoenixjw9320@gmail.com>
2023-02-14 15:30:00 +0000

    fixed 'npm run build error'

--- pages/contact.js
+++ pages/contact.js
@@ -1,21 +1,64 @@
-import Hero from "@/components/homepage/hero"
-import NavigationBar from "@/components/nav-bar"
-import ContactForm from "@/components/contact"
 
-export function Contact() {
-    return (
-      <div className="w-full">
-      <div className="w-full max-w-6xl mx-auto">
-      <NavigationBar></NavigationBar>
-      <Hero name = 'Contact Me'></Hero>
-      <ContactForm></ContactForm>
- 
-      
-    </div>
-    </div>
-  
-     
-    )
-  }
-  
-  export default Contact
+// import Input from "@/components/forms/input";
+// import Content from "@/components/shared/content";
+// import Footer from "@/components/shared/footer";
+// import Header from "@/components/shared/header";
+//  export default function Contact() {
+//     const [message, setMessage] = useState("");
+//     const [showSuccess, setShowSuccess] = useState(false);
+
+//     const handleChangeName = (event) => {
+//         const name = event.target.value;
+//         setName(name);
+//     }
+
+//     const handleChangeEmail = (event) => {
+//         const email = event.target.value;
+//         setEmail(email);
+//     }
+
+//     const handleChangeMessage = (event) => {
+//         const message = event.target.value;
+//         setMessage(message);
+//     }
+//  export default function Contact() {
+
+//             <Content>
+//                 <form className="flex flex-col space-y-3">
+//                     <input
+//                     <Input
+//                         placeholder="Name"
+//                         className="bg-gray-200 py-3 px-6 w-96 rounded"
+//                         value={name}
+//                         onChange={handleChangeName}
+//                         onChange={(value) => setName(value)}
+//                         disabled={true}
+//                     />
+//                     <input
+//                     <Input
+//                         placeholder="Email"
+//                         className="bg-gray-200 py-3 px-6 w-96 rounded"
+//                         value={email}
+//                         onChange={handleChangeEmail}
+//                         onChange={(value) => setEmail(value)}
+//                     />
+//                     <textarea
+//                         placeholder="Message"
+//                         className="bg-gray-200 py-3 px-6 w-96 rounded"
+//                         value={message}
+//                         onChange={handleChangeMessage}
+//                     />
+//                     <button
+//                         className="bg-blue-500 py-3 px-6 w-96 rounded text-white"
+//                         onClick={handleSubmit}
+//                         type="button"
+//                     >
+//                         Submit
+//                     </button>
+//                     {showSuccess === true ? 
+//                         (<p className="text-green-500 w-96">
+//                             Your message was sent!
+//                         </p>)
+//                         :
+//                         null
+//                     }
