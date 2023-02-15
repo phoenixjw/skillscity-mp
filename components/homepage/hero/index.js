5a3ce6e7772c4d741f416238ab97965b813ddc15
phoenixjw <phoenixjw9320@gmail.com>
2023-02-15 11:46:37 +0000

    mobile responsive design for hero component

--- components/homepage/hero/index.js
+++ components/homepage/hero/index.js
@@ -1,16 +1,30 @@
 export default function Hero ({imgURL, name, subtitle}) {
     return (
     <div className="w-full max-w-6xl mx-auto">
-    <div className="w-full flex flex-row space-x-2 ">
-      <div className="w-3/12">
-        <img src={imgURL} className=" mt-5 mb-5 rounded-lg h-36 w-28"/> 
+      <div className="w-full flex flex-col py-4 space-y-4 md:flex-row md:space-x-4">
+        <div className="w-48">
+        <img src={imgURL} className="rounded-lg"/> 
+          
+          
+        </div>
+        <div className="w-full text-center md:text-left">
+        <h1 
+            className = "font-bold text-6xl py-5 text-gray-700 s"
+        >
+          {name}
+        </h1>
+        <p className="text-xl text-gray-700">{subtitle}</p>
+        </div>
+
       </div>
-      <div className="w-9/12">
-      <h1 className = "font-bold text-4xl py-5">{name}</h1>
-      <p className="text-xl text-gray-700">{subtitle}</p>
-      </div>   
     </div>
-    </div>
+
+
+
+
+
+
+
         
     )
 }
