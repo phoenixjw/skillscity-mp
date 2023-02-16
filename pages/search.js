45d4d7e86a6d9335ba728a9ad48b58f9a29bfdfb
phoenixjw <phoenixjw9320@gmail.com>
2023-02-16 12:04:12 +0000

    completed low fidelity UI for search page

--- pages/search.js
+++ pages/search.js
@@ -1,4 +1,30 @@
+import Input from "@/components/Forms/input"
+import NavigationBar from "@/components/nav-bar"
+import { Content } from "@/components/shared/Content"
+
 export function search() {
+    return(
+        <div className="w-full">
+                <NavigationBar></NavigationBar>
+                {/* <p> Add header component</p>   */}
+
+                <Content>
+                    <div className=" w-full md:w-6/12 lg:w-4/12 flex flex-row space-x-2">
+                        <Input/>
+                        <button
+                            className="bg-blue-500 text-white px-3 py-2 rounded-md"
+                            type="button"
+                        ></button>
+                    </div>
+
+                    <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
+                        <div className="w-full h-40 bg-red-100"></div>
+
+
+                    </div>
+                </Content>
+        </div>
+    )
     
 }
 export default search
