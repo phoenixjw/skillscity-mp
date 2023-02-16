d9ad9ef6675ee725b8dcc64910b2e76efb1d2529
phoenixjw <phoenixjw9320@gmail.com>
2023-02-16 19:01:04 +0000

    added locations api endpoint

---
+++ pages/api/locations.js
@@ -0,0 +1,28 @@
+import sql from "@/utils/postgres";
+
+export default async function handler(req,res){
+    const search =req.query.search;
+
+    let locations = [];
+
+    if (search === null || serach === undefined) {
+        locations = await sql `
+
+        select * from locations
+        
+        `
+    }
+
+    else {
+        locations = await sql `
+
+        select * from locations
+        where title = ${search}
+        
+        `;
+    }
+    
+
+
+res.json(locations);
+}
