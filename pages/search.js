8d1dc55c309921857593f25df841ebb050350aea
phoenixjw <phoenixjw9320@gmail.com>
2023-02-20 11:39:51 +0000

    attempt to fix build error

--- pages/search.js
+++ pages/search.js
@@ -1,30 +1,57 @@
-import Input from "@/components/Forms/input"
-import NavigationBar from "@/components/nav-bar"
-import { Content } from "@/components/shared/Content"
+import Input from "@/components/forms/input";
+import NavigationBar from "@/components/nav-bar";
+import LocationItem from "@/components/search/location-item";
+import Content from "@/components/shared/Content";
+import Header from "@/components/shared/Header";
+import { useEffect, useState } from "react";
 
-export function search() {
-    return(
-        <div className="w-full">
-                <NavigationBar></NavigationBar>
-                {/* <p> Add header component</p>   */}
+export default function Search() {
+    const [locations, setLocations] = useState([]);
+    const [input, setInput] = useState("");
 
-                <Content>
-                    <div className=" w-full md:w-6/12 lg:w-4/12 flex flex-row space-x-2">
-                        <Input/>
-                        <button
-                            className="bg-blue-500 text-white px-3 py-2 rounded-md"
-                            type="button"
-                        ></button>
-                    </div>
+    useEffect(() => {
+        getLocations();
+    }, []);
 
-                    <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
-                        <div className="w-full h-40 bg-red-100"></div>
+    const getLocations = async () => {
+        const response = await fetch(`/api/locations`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+
+        const data = await response.json();
+        setLocations(data);
+    }
 
 
-                    </div>
-                </Content>
-        </div>
+    return (
+        // <div className="w-full">
+            <NavigationBar></NavigationBar>
+        //     <Header name="Search" />
+        //     <Content>
+        //         <div className="w-full md:w-6/12 lg:w-4/12 flex flex-row space-x-2">
+        //             <Input
+        //                 value={input}
+        //                 onChange={value => setInput(value)}
+        //             />
+        //             <button
+        //                 className="bg-blue-500 text-white px-6 py-2 rounded-md"
+        //                 type="button"
+        //             >
+        //                 S
+        //             </button>
+        //         </div>
+
+        //         <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
+        //             {locations.map(location => (
+        //                 <LocationItem key={location.id} location={location} />
+        //             ))}                    
+        //         </div>
+
+        //     </Content>
+ 
+        // </div>
     )
-    
 }
-export default search
