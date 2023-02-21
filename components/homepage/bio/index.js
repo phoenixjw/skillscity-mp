39cc038c209a92a9f974b34870ea4e24a7633bfe
phoenixjw <phoenixjw9320@gmail.com>
2023-02-21 21:04:00 +0000

    Edited Bio

--- components/homepage/bio/index.js
+++ components/homepage/bio/index.js
@@ -1,7 +1,9 @@
 export default function Bio(){
     return(
         <div>
-            <p >After working in finance sector as an insolvency administrator and a year studying Economics at University, I made the decision to switch to a career I’m passionate about and make the steps to become a software engineer by joining IN4 Groups Software Engineering bootcamp and enrolling on free Codecademy courses. I have always enjoyed problem solving and upon completion of my Software Engineering bootcamp in March, I feel I’d be ready for my first role in software development as I am starting to build confidence in the foundational skills I have learnt on the bootcamp by applying them to individual mini projects as well as working on a group project as part of the bootcamp.</p>
+            <p>Welcome to my mini project for skillscity! This is a full stack application that has been created using Next.js and tailwind CCS.
+                On each page is are some features I've learnt how to create during the Skillscity software engineering bootcamp. This is my frist experience using a React framework.
+            </p>
         </div>
     )
 }
