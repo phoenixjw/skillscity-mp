3366a0c77c990fa1f00a925fd5f30a7eb2b7b311
phoenixjw <phoenixjw9320@gmail.com>
2023-02-22 15:33:33 +0000

    Fixed typo in footer component

--- components/shared/Footer/index.js
+++ components/shared/Footer/index.js
@@ -4,7 +4,7 @@ export default function Footer({ href, title }) {
     return (
         <div className="w-full max-w-6xl mx-auto mt-8">
             <Link href={href}>
-                Go to {title}
+                {title}
             </Link>
         </div>
     )
