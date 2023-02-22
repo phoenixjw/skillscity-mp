e9b36f8f0737015cce6e630f331669e85b1de8ba
phoenixjw <phoenixjw9320@gmail.com>
2023-02-22 14:30:03 +0000

    dealt with undefined errors for contact messages

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -3,6 +3,21 @@ import sql from "@/utils/postgres";
 export default async function handle(req, res) {
     const { name, email, message } = req.query;
 
+    if (name === undefined) {
+        res.status(400).json({ message: "Name is required" });
+        return;
+    }
+
+    if (email === undefined) {
+        res.status(400).json({ message: "Email is required" });
+        return;
+    }
+
+    if (message === undefined) {
+        res.status(400).json({ message: "Message is required" });
+        return;
+    }
+
     const response = await sql`
         insert into contact_messages
             (name, message, email)
