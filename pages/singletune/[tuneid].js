33800d08e1b54e0ee84e9a54725874cc90b3db9f
phoenixjw <phoenixjw9320@gmail.com>
2023-02-23 22:11:46 +0000

    Removed unused code from tuneid page

--- pages/singletune/[tuneid].js
+++ pages/singletune/[tuneid].js
@@ -27,9 +27,9 @@ export default function SingleTune() {
       const {tune} = data;
       setTunes(tune);
   }
+
   
   console.log(tune)
-//   console.log(tune.title)
   
   if(!tune){
     return (
@@ -47,18 +47,15 @@ export default function SingleTune() {
                 subtitle = {tune.artist}
             />
             
-            <div className="flex flex-row mr-2">
-            <a href={tune.link_to_purchase} target="_blank" className="w-full text-blue-500">
-                Click here to Purhase
-            
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <div className="flex flex-row mr-2 text-blue-500 sm: flex items-center mx-auto">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-4">
             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
             </svg>
+            <a href={tune.link_to_purchase} target="_blank" className="text-blue-500">
+                Click here to Purhase
             </a>
 
             </div>
-
-            
 
 
             <Content>
